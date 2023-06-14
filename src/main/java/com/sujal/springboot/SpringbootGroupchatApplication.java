349f884c45edb138fbc3af38f9bc5684a613fc5c
Sujal Sawdekar <suja7103gm@gmail.com>
2023-06-14 12:19:25 +0530

    Created project

---
+++ src/main/java/com/sujal/springboot/SpringbootGroupchatApplication.java
@@ -0,0 +1,13 @@
+package com.sujal.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGroupchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGroupchatApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sujal/springboot/controllers/MessageController.java
@@ -0,0 +1,29 @@
+package com.sujal.springboot.controllers;
+
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sujal.springboot.models.Message;
+
+@RestController
+public class MessageController {
+
+
+    @MessageMapping("/message")
+    @SendTo("/topic/return-to")
+    public Message getContent(@RequestBody Message message) {
+
+//        try {
+//            //processing
+////            Thread.sleep(2000);
+//
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        return message;
+    }
+
+}
